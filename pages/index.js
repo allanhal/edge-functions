7deef119eaccf05ddc73e382933d939e52898888
Allan Araújo <allan.hal@gmail.com>
2024-11-11 17:25:37 -0300

    jjj

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,13 @@ export default function Home() {
   return (
     <div>
       <h1>Serverless Function Test</h1>
+      <p>environment</p>
+      <WebcamImage facingMode="environment" />
+      <p>exact: environment</p>
       <WebcamImage facingMode={{ exact: "environment" }} />
+      <p>user</p>
       <WebcamImage facingMode="user" />
+      <p>WebcamImageList</p>
       <WebcamImageList />
     </div>
   );
